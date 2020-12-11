9e1adead394bc226842f62f65499e814a2a13c38
drupchen <hhdrupchen@gmail.com>
2020-12-11 04:25:39 +0000

    update

--- scripts/generate.py
+++ scripts/generate.py
@@ -8,17 +8,28 @@ def gen_tables(name, orig, trans):
 
     tables = []
     for line in range(len(orig)):
-        table = [
-            ['\\tr'],
-            ['\\tr'],
-            ['\\tr']
-        ]
+        table = []
         o_els, p_els, w_els = orig[line].split(' '), phonetics[line].split(' '), w2w[line].split(' ')
+        sub_tables = []
+        sub_table = None
         for w in range(len(o_els)):
+            if w % 9 == 0:
+                if sub_table:
+                    sub_tables.append(sub_table)
+                sub_table = [
+                    ['\\tr'],
+                    ['\\tr'],
+                    ['\\tr']
+                ]
             for num, x in enumerate([o_els[w], p_els[w], w_els[w]]):
-                table[num].append(f'\\tc{w+1}')
-                table[num].append(x)
-        tables.append(f'\p\n\\v {line+1}\n{" ".join(table[0])}\n{" ".join(table[1])}\n{" ".join(table[2])}')
+                sub_table[num].append(f'\\tc{(w % 9) + 1}')
+                sub_table[num].append(x)
+        if sub_table:
+            sub_tables.append(sub_table)
+
+        print('')
+        table = '\n\\b\n'.join(['\n'.join([' '.join(a) for a in sub]) for sub in sub_tables])
+        tables.append(f'\p\n\\v {line+1}\n{table}')
 
     total = '\n'.join(tables)
     out_file = Path(f'../USFM/{name}_{trans}_tables.txt')
